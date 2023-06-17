02d593476bd06babf9c3c01f96f4d7c71cce2f1b
LuizArnoldChavezBurgos <luiz.chavez@unmsm.edu.pe>
2023-06-16 23:24:47 -0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from "@/components/navbar/Navbar";
+import Presentation from "@/components/presentation/Presentation";
+
+export default function Home() {
+  return (
+    <main className="container mx-auto">
+      <Navbar />
+      <Presentation />
+    </main>
+  );
+}
